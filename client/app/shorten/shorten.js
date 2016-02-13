0670485d43adf28adc8eb08aa4373a7c38f6e3e6
Griffin Michl <griffinmichl@gmail.com>
2016-02-13 10:20:41 -0800

    add spinner while waiting for url

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -5,9 +5,12 @@ angular.module('shortly.shorten', [])
   $scope.link = {};
   $scope.shortenLink = '';
   $scope.validation = '';
+  $scope.spinner = false;
   $scope.addLink = function () {
+    $scope.spinner = true;
     if ($scope.validation) {
       Links.addLink({url: $scope.shortenLink}).then(function (res) {
+        $scope.spinner = false;
         $scope.link = res.data;
         $scope.hidden = false;
         $scope.shortenLink = '';
